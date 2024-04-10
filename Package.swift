e3cfe95a203ef1c9973de85ee48a771884ab607f
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2024-04-10 19:13:44 -0400

    Whitespace formatting.

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "RestfulCall",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "RestfulCall",
-            targets: ["RestfulCall"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "RestfulCall"),
-        .testTarget(
-            name: "RestfulCallTests",
-            dependencies: ["RestfulCall"]),
-    ]
+	name: "RestfulCall",
+	products: [
+		// Products define the executables and libraries a package produces, making them visible to other packages.
+		.library(
+			name: "RestfulCall",
+			targets: ["RestfulCall"]),
+	],
+	targets: [
+		// Targets are the basic building blocks of a package, defining a module or a test suite.
+		// Targets can depend on other targets in this package and products from dependencies.
+		.target(
+			name: "RestfulCall"),
+		.testTarget(
+			name: "RestfulCallTests",
+			dependencies: ["RestfulCall"]),
+	]
 )
