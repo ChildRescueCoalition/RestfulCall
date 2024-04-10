7ea63612153f6036863a26b84d02dd31db5042e9
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2024-04-10 19:51:30 -0400

    RestfulCall class implementation.

--- Sources/RestfulCall/RestfulCall.swift
+++ Sources/RestfulCall/RestfulCall.swift
@@ -1,2 +1,92 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  RestfulCall.swift
+//  Created by Roberto Machorro on 4/10/24.
+//
+
+import Foundation
+
+class RestfulCall: NSObject {
+	enum RequestMethod: String {
+		case GET
+		case POST
+		case PUT
+		case DELETE
+	}
+
+	enum CallError: Error {
+		case invalidParameters
+		case invalidResponse
+		case invalidMIMEType
+		case invalidHTTPStatus(Int)
+	}
+
+	var baseAddress: String?
+	var token: String?
+	var session: URLSession?
+
+	init(baseAddress: String, ignoreSSL: Bool = false, token: String? = nil) {
+		super.init()
+		self.baseAddress = baseAddress
+		self.token = token
+		self.session = ignoreSSL ?
+			URLSession(configuration: .default, delegate: self, delegateQueue: nil) :
+			URLSession(configuration: .default)
+	}
+
+	func execute(_ operation: RequestMethod = .GET, endpoint path: String, body: Data? = nil, expecting mime: String = "application/json") async throws -> Data {
+		var request = try makeRequest(endpoint: path)
+		request.httpMethod = operation.rawValue
+		if let body {
+			request.httpBody = body
+		}
+		return try await sessionData(with: request, expecting: mime)
+	}
+}
+
+extension RestfulCall {
+	func makeRequest(endpoint path: String) throws -> URLRequest {
+		guard let baseAddress, let url = URL(string: baseAddress + path) else {
+			throw CallError.invalidParameters
+		}
+
+		var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData)
+		request.setValue("application/json", forHTTPHeaderField: "Accept")
+		request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+		if let token {
+			request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+		}
+		return request
+	}
+
+	func sessionData(with request: URLRequest, expecting mimeType: String) async throws -> Data {
+		guard let session else {
+			throw CallError.invalidParameters
+		}
+
+		let result = try await session.data(for: request)
+		guard let response = result.1 as? HTTPURLResponse else {
+			throw CallError.invalidResponse
+		}
+		guard (200...299).contains(response.statusCode) else {
+			throw CallError.invalidHTTPStatus(response.statusCode)
+		}
+		guard response.mimeType == mimeType else {
+			throw CallError.invalidMIMEType
+		}
+
+		return result.0
+	}
+}
+
+extension RestfulCall: URLSessionDelegate {
+	// swiftlint:disable:next line_length
+	func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge) async -> (URLSession.AuthChallengeDisposition, URLCredential?) {
+		if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
+			guard let serverTrust = challenge.protectionSpace.serverTrust else {
+				return (.useCredential, nil)
+			}
+			return (.useCredential, URLCredential(trust: serverTrust))
+		}
+		return (.performDefaultHandling, nil)
+	}
+}
