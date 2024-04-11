876ed703c7f3121c2d3b90421691bdc9b1faeac7
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2024-04-10 20:03:41 -0400

    Downgraded Swift to 5.7 and macOS to v12.
    Github has older tools installed for Actions.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
 	name: "RestfulCall",
 	platforms: [
-		.macOS(.v13)
+		.macOS(.v12)
 	],
 	products: [
 		// Products define the executables and libraries a package produces, making them visible to other packages.
