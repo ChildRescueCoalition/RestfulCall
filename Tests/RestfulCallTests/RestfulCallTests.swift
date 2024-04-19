49566de734861b94eb069ba6b8e6becef784f664
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2024-04-18 21:34:35 -0400

    Cleaner testing.

--- Tests/RestfulCallTests/RestfulCallTests.swift
+++ Tests/RestfulCallTests/RestfulCallTests.swift
@@ -1,38 +1,33 @@
 import XCTest
 @testable import RestfulCall
 
-struct EchoResponse: Decodable {
+struct HTTPBinResponse: Decodable {
+	// let args: [:]
 	let headers: [String: String]
-	let method: String
-	let `protocol`: String
-	let path: String
-	let body: String
-	let hostname: String
-	let ip: String
-	let ips: [String]
-	let xhr: Bool
-	let fresh: Bool
+	let origin: String
+	let url: String
 }
 
 final class RestfulCallTests: XCTestCase {
-	func testFetchIPfromGridcop() async throws {
-		let gridcopTest = RestfulCall(baseAddress: "https://test.gridcop.com", token: "fakeTOKEN")
-		let result = try await gridcopTest.execute(.GET, endpoint: "/")
-		let info = try JSONDecoder().decode(EchoResponse.self, from: result)
+	func testHTTPBinGet() async throws {
+		let httpBin = RestfulCall(baseAddress: "https://httpbin.org", token: "fakeTOKEN")
+		let result = try await httpBin.execute(.GET, endpoint: "/get")
+		let response = try JSONDecoder().decode(HTTPBinResponse.self, from: result)
 
-		XCTAssertEqual(info.method, "GET")
-		XCTAssertTrue(info.ip.count > 0)
+		XCTAssertEqual(response.url, "https://httpbin.org/get")
+		XCTAssertTrue(response.headers.count > 0)
+		XCTAssertEqual(response.headers["Host"], "httpbin.org")
 	}
 
 	func testIncompatibleMIME() async throws {
-		let gridcopTest = RestfulCall(baseAddress: "https://test.gridcop.com", token: "fakeTOKEN")
+		let httpBin = RestfulCall(baseAddress: "https://httpbin.org", token: "fakeTOKEN")
 		do {
-			_ = try await gridcopTest.execute(.GET, endpoint: "/", expecting: "text/html")
+			_ = try await httpBin.execute(.GET, endpoint: "/get", expecting: "text/html")
 		}
 		catch let callError as RestfulCall.CallError {
 			switch callError {
 			case let .invalidMIMEType(mime):
-				print("MIME: \(mime)")
+				XCTAssertEqual("application/json", mime)
 			default:
 				XCTFail("Wrong exception thrown")
 			}
@@ -40,14 +35,14 @@ final class RestfulCallTests: XCTestCase {
 	}
 
 	func testWithoutMIME() async throws {
-		let gridcopTest = RestfulCall(baseAddress: "https://test.gridcop.com", token: "fakeTOKEN")
+		let httpBin = RestfulCall(baseAddress: "https://httpbin.org", token: "fakeTOKEN")
 		do {
-			_ = try await gridcopTest.execute(.GET, endpoint: "/", expecting: nil)
+			_ = try await httpBin.execute(.GET, endpoint: "/get", expecting: nil)
 		}
 		catch let callError as RestfulCall.CallError {
 			switch callError {
 			case let .invalidMIMEType(mime):
-				print("MIME: \(mime)")
+				XCTAssertEqual("application/json", mime)
 			default:
 				XCTFail("Wrong exception thrown")
 			}
